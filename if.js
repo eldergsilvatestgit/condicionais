52298bd51bd497ef265df27700ee2be961515d72
Elder Gomes <eldergomesdas@gmail.com>
2026-03-02 09:40:43 -0300

    operador if

---
+++ if.js
@@ -0,0 +1,12 @@
+const  idade = 9 ;
+
+if ( idade >= 18){
+    // é executado caso a condição seja verdadeira
+    console.log(`è maior de idade `);
+}
+
+
+ console.log(` ...fim do codigo`);
+
+
+ 
